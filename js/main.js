6bed5e96188a3200f8faa30fc9db69921436ffa2
Parker Jones <pjones@progresstesting.com>
2013-07-09 18:15:50 -0400

    fixed the issue with the javascript stack, added validation to data model

--- js/main.js
+++ js/main.js
@@ -4,8 +4,13 @@ v 0.0.1
 	Defining dat structures that will eventually hold staar download items
  */
 
-var Person = function(config){
-	this.name = config.name;
-	this.age = config.age;
-	this.occupation = config.occupation;
-};
+var Person = Backbone.Model.extend({
+    defaults: {
+        name: 'Guest User',
+        age: 23,
+        occupation: 'Worker'
+    },
+    work: function(){
+        return this.get('name') + ' is working.';
+    }
+});
