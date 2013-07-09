b7d1693009f5bf3b7526fc289de85e449ef83fde
Parker Jones <pjones@progresstesting.com>
2013-07-09 19:59:11 -0400

    moved template to index.html, added template, have not yet removed template from view class

--- js/main.js
+++ js/main.js
@@ -8,9 +8,25 @@ var Person = Backbone.Model.extend({
     defaults: {
         name: 'Guest User',
         age: 23,
-        occupation: 'Worker'
-    },
-    work: function(){
-        return this.get('name') + ' is working.';
+        occupation: 'worker'
     }
 });
+
+var PersonView = Backbone.View.extend({
+   tagName: 'li',
+
+   my_template: _.template("<strong><%= name %></strong> (<%= age %>) - <%= occupation %>"),
+
+   initialize: function(){
+     this.render();
+   },
+
+   render: function(){
+     this.$el.html( this.my_template(this.model.toJSON()));
+  }
+});
+
+// calls from console
+
+// var person = new Person;
+// var personView = new PersonView({ model: person });
