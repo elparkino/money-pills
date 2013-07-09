291d51a191a00121078c8209530e5758f84d91b2
Parker Jones <pjones@progresstesting.com>
2013-07-09 17:24:49 -0400

    pushing the files, starting this train

---
+++ js/main.js
@@ -0,0 +1,11 @@
+/*
+This is a file,
+v 0.0.1
+	Defining dat structures that will eventually hold staar download items
+ */
+
+var Person = function(config){
+	this.name = config.name;
+	this.age = config.age;
+	this.occupation = config.occupation;
+};
