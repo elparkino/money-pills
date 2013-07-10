f30b3290e8cb7c4d993d51abbddcc39898c8977f
Parker Jones <pjones@progresstesting.com>
2013-07-10 18:41:48 -0400

    I did a whole bunch of stuff.  We have a skeleton framework now, but no drag and drop and no download and no dynamic data modling yet.  It's a start... three days and counting

--- index.php
+++ index.php
@@ -1,37 +1,82 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-    <head>
-        <title>ZipDrop | Drag, Drop, Zip, Download!</title>
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/styles/main.css" type="text/css"/>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet">
+    <link rel="author" href="https://plus.google.com/u/0/102941304163335429492/">	
+	<title>Teacher's Staar Test Backpack</title>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
 
-        <script type="text/javascript" src="assets/scripts/jquery.js" ></script>
-        <script type="text/javascript" src="assets/scripts/jqueryui.js" ></script>
-        <script type="text/javascript" src="assets/scripts/zipdrop.js" ></script>
+</head>
 
-        <link rel="stylesheet" href="assets/styles/main.css" type="text/css"/>
-    </head>
-    <body>
-        <div id="container">
-            <div id="downloadable_files">
-                <h1>Files</h1>
-                <p>Grab a file and drag it to the <em>downloads</em> area</p>
-                <div id="thumbs">
-                    <div id="1" class="thumb_container"><a href="assets/files/downloads/leafs.jpg"><img class="downloadable" src="assets/files/thumbs/leafs.jpg" alt="leafs.jpg" /><br /><span>Leafs</span></a></div>
-                    <div id="2" class="thumb_container"><a href="assets/files/downloads/road.jpg"><img class="downloadable" src="assets/files/thumbs/road.jpg" alt="road.jpg" /><br /><span>Road</span></a></div>
-                    <div id="3" class="thumb_container"><a href="assets/files/downloads/mountains.jpg"><img class="downloadable" src="assets/files/thumbs/mountains.jpg" alt="mountains.jpg" /><br /><span>Mountains</span></a></div>
-                    <div id="4" class="thumb_container"><a href="assets/files/downloads/playtime.jpg"><img class="downloadable" src="assets/files/thumbs/playtime.jpg" alt="playtime.jpg" /><br /><span>Playtime</span></a></div>
-                    <div id="5" class="thumb_container"><a href="assets/files/downloads/tree.jpg"><img class="downloadable" src="assets/files/thumbs/tree.jpg" alt="tree.jpg" /><br /><span>Tree</span></a></div>
-                </div>
-            </div>
-            <div id="download_list">
-                <h1>Downloads</h1>
-                <div id="info"></div>
-                <h4 id="file_title"></h4>
-                <ul id="file_ul">
 
-                </ul>
-                <a id="zipdrop_download" style="display:none" href="#">Download</a>
-            </div>
-            <br style="clear:both;" />
-        </div>
-    </body>
+<body>
+	<div id="output"></div>
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="offset2 span8"id="download_instructions">
+					<h1>Please select the items to fill your STAAR Teachers' Backpack</h1>
+					<p>Grab a file and drag it to the <em>downloads</em> area</p>
+				</div>
+			</div>			
+		</div>
+
+		<div id="container" class="container-fluid">
+
+			<div class="row-fluid">
+
+				<div class= "span6" id="files-container">
+
+
+					<div class="row-fluid">
+        				<div class="span12" id="downloadable_files">
+                			<div id="thumbs">
+								<div id="1" class="draggable thumb_container">
+									<a class="downloadable" href="assets/sampleTests/STAAR4Math.pdf"><img class="downloadable" src="assets/img/img.png" alt="leafs.jpg" /><br />Staar 4 Math</a>
+								</div>
+							</div>	
+						</div>
+					</div>
+				</div>
+
+				<div class="span6">
+
+					<div class="row-fluid headr" id="download_head">
+						<h1 class="offset2 span8">Downloads</h1>	
+					</div>
+
+					<div clas="row-fluid">
+						<div class="span12" id="download_list">
+	                	
+	                		<div class="span12" id="info"></div>
+	                		<h4 id="file_title"></h4>
+	                		<ul id="file_ul">
+
+	                		</ul>
+	                		<a id="zipdrop_download" href="#">Download</a>
+	            		</div>
+	            	</div>
+				</div>
+
+			</div>
+		</div>
+
+</body>
+
+
+
+<footer>
+<script id="itemTemplate" type="text/template">
+    <a href="<%= name %>"</a><strong><%= name %></strong> (Grade: <%= grade %>) - Academic Rigor: <%= Rigor %>
+</script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4/underscore-min.js"></script>
+<script src="http://backbonejs.org/backbone-min.js"></script>
+<script type="text/javascript" src="assets/scripts/zipdrop.js" ></script>
+<script src="js/main.js"></script>
+<footer>
 </html>
