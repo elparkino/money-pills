77b9caebb2521d5ecbf416c2270b780229387cbc
Parker <parker.emailaddress@gmail.com>
2013-07-15 18:14:05 -0400

    wrote function that contains the API call to wordpress. wrote function that takes that api call and iterates through the resonse and builds variables to pass to the collection

--- js/main.js
+++ js/main.js
@@ -67,108 +67,40 @@ var ItemView = Backbone.View.extend({
 
 });
 
+function get_wordpress_attachments(url){
+
+    var pt_json = $.getJSON(url + "get_page&page_id=22&include=attachments");
+    var items = pt_json.responseJSON.page.attachments;
+
+    return items;
+}
+
+function parse_items(){
+
+    var pt_url = "https://parker-jones.org/staar/?json=";
+
+    var sample_array = get_wordpress_attachments(pt_url);
+
+    var item_url = [];
+    var title_url = [];
+
+    for(var i=0; i<(items.length-1); i++){
+
+        var item_url[i] = sample_array.url;
+        var title_url[i] = sample_array.title;
+    }
+
+
+}
+
+
+
 var downloadItems = new DownloadItems([
     {
         subject: 'STAAR Math',
         gradeLevel: '4',
         language: 'English',
         file_path: 'assets/sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Science',
-        gradeLevel: '4',
-        language: 'English',
-        file_path: 'assets/sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Reading',
-        gradeLevel: '5',
-        language: 'English',
-        file_path: 'sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Math',
-        gradeLevel: '4',
-        language: 'English',
-        file_path: 'assets/sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Science',
-        gradeLevel: '4',
-        language: 'English',
-        file_path: 'assets/sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Reading',
-        gradeLevel: '5',
-        language: 'English',
-        file_path: 'sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Math',
-        gradeLevel: '4',
-        language: 'English',
-        file_path: 'assets/sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Science',
-        gradeLevel: '4',
-        language: 'English',
-        file_path: 'assets/sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Reading',
-        gradeLevel: '5',
-        language: 'English',
-        file_path: 'sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Math',
-        gradeLevel: '4',
-        language: 'English',
-        file_path: 'assets/sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Science',
-        gradeLevel: '4',
-        language: 'English',
-        file_path: 'assets/sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-    {
-        subject: 'STAAR Reading',
-        gradeLevel: '5',
-        language: 'English',
-        file_path: 'sampleTests/staarsample.pdf',
-        url: 'localhost/backapp/',
-        img_url: 'assets/img/img.png'
-    },
-        {
-        subject: 'STAAR US history',
-        gradeLevel: '8',
-        language: 'English & Spanish',
-        file_path: 'sampleTests/staarsample.pdf',
         url: 'localhost/backapp/',
         img_url: 'assets/img/img.png'
     }
